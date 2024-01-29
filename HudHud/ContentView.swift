acbcb585c0a359b5ffc66eab89d623d57b0a83b1
Patrick Kladek <patrick.kladek@outlook.com>
2024-01-29 21:05:41 +0400

    Initial Commit

---
+++ HudHud/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HudHud
+//
+//  Created by Patrick Kladek on 29.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HudHud/HudHudApp.swift
@@ -0,0 +1,17 @@
+//
+//  HudHudApp.swift
+//  HudHud
+//
+//  Created by Patrick Kladek on 29.01.24.
+//
+
+import SwiftUI
+
+@main
+struct HudHudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
