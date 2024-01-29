5757f4784d09b5d361da6c89aac1802b449800e6
Patrick Kladek <patrick.kladek@outlook.com>
2024-01-29 21:28:01 +0400

    add organisation & fix copyright header

--- HudHud/HudHudApp.swift
+++ HudHud/HudHudApp.swift
@@ -3,15 +3,16 @@
 //  HudHud
 //
 //  Created by Patrick Kladek on 29.01.24.
+//  Copyright © 2024 HudHud. All rights reserved.
 //
 
 import SwiftUI
 
 @main
 struct HudHudApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
 }
