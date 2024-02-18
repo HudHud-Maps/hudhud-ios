4f44aed34cdb204c91ac0a934dfee6a5b6695680
PW <patrick@subzero.eu>
2024-02-18 16:54:28 +0100

    test changes

--- HudHudUITests/HudHudUITests.swift
+++ HudHudUITests/HudHudUITests.swift
@@ -23,20 +23,16 @@ final class HudHudUITests: XCTestCase {
 		// Put teardown code here. This method is called after the invocation of each test method in the class.
 	}
 
-	func testExample() throws {
+	func testForSearchField() throws {
 		// UI tests must launch the application that they test.
 		let app = XCUIApplication()
 		app.launch()
 
-		// Use XCTAssert and related functions to verify your tests produce the correct results.
+		// Identify the TextField with placeholder "Search"
+		let searchTextField = app.textFields["Search"]
+		
+		// Assert that the TextField exists
+		XCTAssertTrue(searchTextField.exists, "The TextField with placeholder 'Search' does not exist.")
 	}
 
-	func testLaunchPerformance() throws {
-		if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-			// This measures how long it takes to launch your application.
-			measure(metrics: [XCTApplicationLaunchMetric()]) {
-				XCUIApplication().launch()
-			}
-		}
-	}
 }
