3cf30578a66aadff0da6718c035b7cb172884dd3
Patrick Kladek <patrick.kladek@outlook.com>
2024-02-26 00:05:36 +0400

    show all pois on map

--- HudHud/Model/MapStore.swift
+++ HudHud/Model/MapStore.swift
@@ -6,9 +6,12 @@
 //  Copyright © 2024 HudHud. All rights reserved.
 //
 
+import CoreLocation
 import Foundation
 import MapLibreSwiftUI
 import SwiftUI
+
+// MARK: - MapStore
 
 // Anything called "Store" will always be a class and an ObservableObject
 class MapStore: ObservableObject {
@@ -19,10 +22,40 @@ class MapStore: ObservableObject {
 		didSet {
 			if let coordinate = self.mapItemStatus.selectedItem?.locationCoordinate {
 				self.camera = .center(coordinate, zoom: 16)
+				return
+			}
+
+			let coordinates = self.mapItemStatus.mapItems.compactMap(\.coordinate)
+			if let camera = CameraState.boundingBox(from: coordinates) {
+				self.camera = camera
 			}
 		}
 	}
 
 	@Published var camera = MapViewCamera.center(.riyadh, zoom: 10)
 	@Published var searchShown: Bool = true
+}
+
+extension CameraState {
+
+	static func boundingBox(from locations: [CLLocationCoordinate2D]) -> MapViewCamera? {
+		guard !locations.isEmpty else { return nil }
+
+		var minLat = locations[0].latitude
+		var maxLat = locations[0].latitude
+		var minLon = locations[0].longitude
+		var maxLon = locations[0].longitude
+
+		for coordinate in locations {
+			minLat = min(minLat, coordinate.latitude)
+			maxLat = max(maxLat, coordinate.latitude)
+			minLon = min(minLon, coordinate.longitude)
+			maxLon = max(maxLon, coordinate.longitude)
+		}
+
+		let northeast = CLLocationCoordinate2D(latitude: maxLat, longitude: maxLon)
+		let southwest = CLLocationCoordinate2D(latitude: minLat, longitude: minLon)
+
+		return .boundingBox(northeast: northeast, southwest: southwest, zoom: 10)
+	}
 }
