4cc9d5460fbb507701b7d2322ca35f16bcf8068d
AlloTheOne <allo.busy@gmail.com>
2024-02-27 12:23:42 +0300

    favortie categories UI second attempt

---
+++ HudHud/Views/FavoriteCategories/FavoriteCategoriesButton.swift
@@ -0,0 +1,32 @@
+//
+//  FavoriteCategoriesButton.swift
+//  HudHud
+//
+//  Created by Alaa . on 27/02/2024.
+//  Copyright © 2024 HudHud. All rights reserved.
+//
+
+import SwiftUI
+import SFSafeSymbols
+
+struct FavoriteCategoriesButton: ButtonStyle {
+	let sfSymbol: String?
+	let tintColor: Color?
+	func makeBody(configuration: Configuration) -> some View {
+		VStack {
+			Image(systemSymbol: SFSymbol(rawValue: sfSymbol ?? "house.fill"))
+				.font(.title)
+				.foregroundColor(tintColor)
+				.padding(15)
+				.background {
+					Circle()
+						.foregroundColor(.white)
+						.shadow(color: .black.opacity(0.15), radius: 10, y: 10)
+				}
+			configuration.label
+				.tint(.primary)
+				.lineLimit(1)
+				.minimumScaleFactor(0.5)
+		}
+	}
+}
---
+++ HudHud/Views/FavoriteCategories/FavoriteCategoriesData.swift
@@ -0,0 +1,16 @@
+//
+//  FavoriteCategoriesData.swift
+//  HudHud
+//
+//  Created by Alaa . on 27/02/2024.
+//  Copyright © 2024 HudHud. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct FavoriteCategoriesData: Hashable {
+	let title: String
+	let sfSymbol: String
+	let tintColor: Color?
+}
