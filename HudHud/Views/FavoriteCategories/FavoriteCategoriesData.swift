9debe509c82f4964fa9fcae085de7df81576b335
Alaa Alabdullah <79634177+AlloTheOne@users.noreply.github.com>
2024-02-27 19:38:11 +0300

    HHIOS-36 Favorite categories UI second attempt (#25)
    * favortie categories UI second attempt
    
    * changed sfSymbol from string to SFSymbol

---
+++ HudHud/Views/FavoriteCategories/FavoriteCategoriesData.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteCategoriesData.swift
+//  HudHud
+//
+//  Created by Alaa . on 27/02/2024.
+//  Copyright © 2024 HudHud. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import SFSafeSymbols
+
+struct FavoriteCategoriesData: Hashable {
+	let title: String
+	let sfSymbol: SFSymbol
+	let tintColor: Color?
+}
