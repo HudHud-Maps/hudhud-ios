82f5c0b946d6db94b90f59b6276ebdf80b54a82d
AlloTheOne <allo.busy@gmail.com>
2024-02-27 13:39:18 +0300

    created the file

---
+++ HudHud/Views/MapButtonsView.swift
@@ -0,0 +1,19 @@
+//
+//  MapButtonsView.swift
+//  HudHud
+//
+//  Created by Alaa . on 27/02/2024.
+//  Copyright © 2024 HudHud. All rights reserved.
+//
+
+import SwiftUI
+
+struct MapButtonsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapButtonsView()
+}
