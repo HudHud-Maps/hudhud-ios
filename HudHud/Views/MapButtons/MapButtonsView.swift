b7799f250a81f4514ddb3a77a4014518b37c86d7
AlloTheOne <allo.busy@gmail.com>
2024-03-03 20:25:56 +0300

    HHIOS-46 Map Buttons UI

---
+++ HudHud/Views/MapButtons/MapButtonsView.swift
@@ -0,0 +1,47 @@
+//
+//  MapButton.swift
+//  HudHud
+//
+//  Created by Alaa . on 03/03/2024.
+//  Copyright © 2024 HudHud. All rights reserved.
+//
+
+import SwiftUI
+
+struct MapButtonsView: View {
+	@State var mapButtonsData: [MapButtonData]
+    var body: some View {
+		VStack(spacing: 0) {
+			ForEach(mapButtonsData.indices, id: \.self) { index in
+				Button(action: mapButtonsData[index].action) {
+					Image(systemSymbol:  mapButtonsData[index].sfSymbol)
+						.font(.title)
+						.padding(.vertical)
+						.padding(.horizontal, 10)
+						.foregroundColor(.gray)
+				}
+				if index != mapButtonsData.count - 1 {
+					Divider()
+				}
+							}
+						}
+						.background(Color.white)
+						.cornerRadius(15)
+						.shadow(color: .black.opacity(0.1), radius: 10, y: 4)
+						.fixedSize()
+    }
+}
+
+#Preview {
+    MapButtonsView(mapButtonsData: [
+		MapButtonData(sfSymbol: .map) {
+	  print("Map button tapped")
+  },
+		MapButtonData(sfSymbol: .location) {
+	  print("Location button tapped")
+  },
+		MapButtonData(sfSymbol: .cube) {
+	  print("Location button tapped")
+  }
+])
+}
