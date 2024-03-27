8d5dc5f5cf979b75446bf993ea8ea27ee8bbd775
Fatima Aljaber <fatima-aljaber@hotmail.com>
2024-03-27 16:01:36 +0300

    hhios-69 basic navigation styling (#51)
    * HHIOS-70
    
    * updated
    
    * feature/HHIOS-69-Basic-Navigation-Styling
    
    * Update NavigationView.swift
    
    * Update MapStyles.swift

---
+++ HudHud/Views/MapStyles.swift
@@ -0,0 +1,75 @@
+//
+//  MapStyles.swift
+//  HudHud
+//
+//  Created by Fatima Aljaber on 25/03/2024.
+//  Copyright © 2024 HudHud. All rights reserved.
+//
+
+import Foundation
+import MapboxCoreNavigation
+import MapboxDirections
+import MapboxNavigation
+import MapLibre
+import MapLibreSwiftDSL
+import MapLibreSwiftUI
+import SwiftUI
+
+// MARK: - CustomDayStyle
+
+class CustomDayStyle: DayStyle {
+
+	// MARK: - Lifecycle
+
+	required init() {
+		super.init()
+		//	mapStyleURL = URL(string: "mapbox://styles/mapbox/satellite-streets-v9")!
+		styleType = .day
+	}
+
+	// MARK: - Internal
+
+	override func apply() {
+		super.apply()
+		InstructionsBannerView.appearance().backgroundColor = .systemGreen
+		InstructionsBannerContentView.appearance().backgroundColor = .systemGreen
+		DistanceLabel.appearance(whenContainedInInstancesOf: [InstructionsBannerView.self]).unitTextColor = .white
+		DistanceLabel.appearance(whenContainedInInstancesOf: [InstructionsBannerView.self]).valueTextColor = .white
+		DistanceLabel.appearance(whenContainedInInstancesOf: [InstructionsBannerView.self]).normalTextColor = .white
+		DistanceLabel.appearance(whenContainedInInstancesOf: [InstructionsBannerView.self]).textColor = .white
+		PrimaryLabel.appearance(whenContainedInInstancesOf: [InstructionsBannerView.self]).normalTextColor = .white
+		SecondaryLabel.appearance(whenContainedInInstancesOf: [InstructionsBannerView.self]).normalTextColor = .white
+		ManeuverView.appearance().backgroundColor = .clear
+		ManeuverView.appearance(whenContainedInInstancesOf: [InstructionsBannerView.self]).primaryColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+		ManeuverView.appearance(whenContainedInInstancesOf: [InstructionsBannerView.self]).secondaryColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.3)
+	}
+}
+
+// MARK: - CustomNightStyle
+
+class CustomNightStyle: NightStyle {
+
+	// MARK: - Lifecycle
+
+	required init() {
+		super.init()
+		styleType = .night
+	}
+
+	// MARK: - Internal
+
+	override func apply() {
+		super.apply()
+		InstructionsBannerView.appearance().backgroundColor = .systemGreen
+		InstructionsBannerContentView.appearance().backgroundColor = .systemGreen
+		DistanceLabel.appearance(whenContainedInInstancesOf: [InstructionsBannerView.self]).unitTextColor = .white
+		DistanceLabel.appearance(whenContainedInInstancesOf: [InstructionsBannerView.self]).valueTextColor = .white
+		DistanceLabel.appearance(whenContainedInInstancesOf: [InstructionsBannerView.self]).normalTextColor = .white
+		DistanceLabel.appearance(whenContainedInInstancesOf: [InstructionsBannerView.self]).textColor = .white
+		PrimaryLabel.appearance(whenContainedInInstancesOf: [InstructionsBannerView.self]).normalTextColor = .white
+		SecondaryLabel.appearance(whenContainedInInstancesOf: [InstructionsBannerView.self]).normalTextColor = .white
+		ManeuverView.appearance().backgroundColor = .clear
+		ManeuverView.appearance(whenContainedInInstancesOf: [InstructionsBannerView.self]).primaryColor = #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)
+		ManeuverView.appearance(whenContainedInInstancesOf: [InstructionsBannerView.self]).secondaryColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.3)
+	}
+}
