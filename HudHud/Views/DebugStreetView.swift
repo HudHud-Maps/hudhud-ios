522f12be561be084127930efcbb19648c116b446
Patrick Kladek <patrick.kladek@outlook.com>
2024-03-28 23:09:01 +0400

    add gesture recognizer to debugView

--- HudHud/Views/DebugStreetView.swift
+++ HudHud/Views/DebugStreetView.swift
@@ -12,72 +12,22 @@ import SwiftUI
 // MARK: - MotionViewModel
 
 class MotionViewModel: ObservableObject {
-	private var motionManager: CMMotionManager
-	private var lastPositions: [Position] = []
-	private let smoothingPasses = 10
-
-	@Published var motion: CMDeviceMotion?
-	@Published var position: Position?
-
-	// MARK: - Lifecycle
-
-	init() {
-		self.motionManager = CMMotionManager()
-		self.motionManager.deviceMotionUpdateInterval = 1.0 / Double(UIScreen.main.maximumFramesPerSecond)
-		self.start()
-	}
-
-	deinit {
-		self.motionManager.stopGyroUpdates()
-	}
+	@Published var position: Position = .zero
+	@Published var positionOffet: Position?
 
 	// MARK: - Internal
 
-	@objcMembers
-	final class Position: NSObject, Averageable {
+	struct Position: Equatable {
 		var heading: Double
 		var pitch: Double
 
 		static var zero: MotionViewModel.Position = .init(heading: 0, pitch: 0)
 
-		// MARK: - Lifecycle
-
-		init(heading: Double, pitch: Double) {
-			self.heading = heading
-			self.pitch = pitch
-		}
-
 		// MARK: - Internal
 
-		override func copy() -> Any {
-			return Position(heading: self.heading, pitch: self.pitch)
-		}
-	}
-
-	// MARK: - Private
-
-	private func start() {
-		guard self.motionManager.isDeviceMotionAvailable else {
-			print("Gyroscope is not available on this device.")
-			return
-		}
-
-		self.motionManager.startDeviceMotionUpdates(using: .xArbitraryCorrectedZVertical, to: .main) { [weak self] motionData, error in
-			guard let self else { return }
-			guard error == nil, let motionData else {
-				print("Error reading gyroscope: \(error?.localizedDescription ?? "<nil>")")
-				return
-			}
-			self.motion = motionData
-
-			let newPosition = Position(heading: motionData.attitude.yaw.toDegrees(),
-									   pitch: motionData.attitude.pitch)
-			self.lastPositions.append(newPosition)
-
-			if self.lastPositions.count >= self.smoothingPasses {
-				self.lastPositions.removeFirst()
-				self.position = self.lastPositions.average()
-			}
+		static func + (left: Position, right: Position) -> Position {
+			return Position(heading: left.heading + right.heading,
+							pitch: left.pitch + right.pitch)
 		}
 	}
 }
@@ -88,27 +38,30 @@ struct DebugStreetView: View {
 
 	@ObservedObject var viewModel = MotionViewModel()
 
+//	@State private var dragStartOffset: CGSize?
+//	@State var dragOffset: CGSize = .zero
+
 	var body: some View {
 		VStack {
 			Text("Gyroscope Data")
-//			if let position = self.viewModel.position {
-//				Text("Heading: \(String(format: "%4.1f", position.heading))")
-//					.monospaced()
-//				Text("Pitch: \(String(format: "%4.1f", position.pitch))")
-//					.monospaced()
-			if let motion = self.viewModel.motion {
-				Text("X: \(String(format: "%4.3f", motion.attitude.quaternion.x))")
-					.monospaced()
-				Text("Y: \(String(format: "%4.3f", motion.attitude.quaternion.y))")
-					.monospaced()
-				Text("Z: \(String(format: "%4.3f", motion.attitude.quaternion.z))")
-					.monospaced()
-				Text("W: \(String(format: "%4.3f", motion.attitude.quaternion.w))")
-					.monospaced()
-			} else {
-				Text("Loading")
-			}
+			Text("Heading: \(String(format: "%4.1f", self.viewModel.position.heading))")
+				.monospaced()
+			Text("Pitch: \(String(format: "%4.1f", self.viewModel.position.pitch))")
+				.monospaced()
 		}
+		.gesture(DragGesture(minimumDistance: 10, coordinateSpace: .global)
+			.onChanged { value in
+				if let dragStartOffset = viewModel.positionOffet {
+					self.viewModel.position = dragStartOffset + MotionViewModel.Position(heading: value.translation.width,
+																						 pitch: value.translation.height)
+				} else {
+					self.viewModel.positionOffet = self.viewModel.position
+				}
+			}
+			.onEnded { _ in
+				self.viewModel.positionOffet = nil
+			}
+		)
 	}
 }
 
