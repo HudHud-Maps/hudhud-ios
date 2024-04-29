44f8d9e8b4cedd80a99a5b1d4555ca55eb2db1d9
Fatima Aljaber <fatima-aljaber@hotmail.com>
2024-04-29 13:32:56 +0300

    mapbox package
    This branch link to https://github.com/FatooAJ/mapbox-directions-swift

--- HudHud/Views/NavigationView.swift
+++ HudHud/Views/NavigationView.swift
@@ -41,7 +41,7 @@ public struct NavigationView: UIViewControllerRepresentable {
 		// simulatedLocationManager.speedMultiplier = 1
 
 		let routeVoice = RouteVoiceController()
-		let directions = Directions(accessToken: nil, host: "gh.maptoolkit.net")
+		let directions = Directions(host: "gh.maptoolkit.net")
 		let navigationController = NavigationViewController(for: self.route, directions: directions, styles: [CustomDayStyle(), CustomNightStyle()], voiceController: routeVoice)
 		navigationController.mapView?.styleURL = self.styleURL
 		navigationController.mapView?.logoView.isHidden = true
