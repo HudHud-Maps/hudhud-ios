5cded5fff7d61e4ef368e26d7527529b60ce6031
AlloTheOne <Allo.busy@gmail.com>
2024-06-03 12:19:40 +0300

    HHIOS-137-Add-Favorites-Part1

---
+++ HudHud/Views/FavoriteCategories/EditFavoritesFormView.swift
@@ -0,0 +1,19 @@
+//
+//  EditFavoritesFormView.swift
+//  HudHud
+//
+//  Created by Alaa . on 03/06/2024.
+//  Copyright © 2024 HudHud. All rights reserved.
+//
+
+import SwiftUI
+
+struct EditFavoritesFormView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditFavoritesFormView()
+}
---
+++ HudHud/Views/FavoriteCategories/FavoritesViewMoreView.swift
@@ -0,0 +1,19 @@
+//
+//  FavoritesViewMoreView.swift
+//  HudHud
+//
+//  Created by Alaa . on 03/06/2024.
+//  Copyright © 2024 HudHud. All rights reserved.
+//
+
+import SwiftUI
+
+struct FavoritesViewMoreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritesViewMoreView()
+}
