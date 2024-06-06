b951c79a637feeb2ba9cbae42a739e6f05ca3578
PW <patrick@subzero.eu>
2024-06-06 13:14:24 +0200

    fixing removed safe area inset

--- HudHud/Views/ContentView.swift
+++ HudHud/Views/ContentView.swift
@@ -152,7 +152,7 @@ struct ContentView: View {
                 self.searchViewStore.mapStore.selectedItem = selectedItem
             }
         })
-
+        .backport.safeAreaPadding(.bottom, self.sheetSize.height)
         .onChange(of: self.mapStore.routes) { newRoute in
             if let routeUnwrapped = newRoute {
                 if let route = routeUnwrapped.routes.first, let coordinates = route.coordinates, !coordinates.isEmpty {
