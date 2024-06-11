28e6c11e1a407f71e342833ec7fbcb4d83e386ca
Fatima Aljaber <fatima-aljaber@hotmail.com>
2024-06-11 12:53:38 +0300

    Update TrendingStore.swift

--- BackendService/Sources/Toursprung/TrendingStore.swift
+++ BackendService/Sources/Toursprung/TrendingStore.swift
@@ -17,17 +17,17 @@ public class TrendingStore: ObservableObject{
     @Published public var lastError: Error?
     
     public func getTrendingPOIs(page: Int, limit: Int, coordinates: CLLocationCoordinate2D?) async throws -> [ResolvedItem] {
-        let client = Client(serverURL: URL(string: "https://hudhud.sa")!, transport: URLSessionTransport())
+        let client = Client(serverURL: URL(string: "https://api.dev.hudhud.sa")!, transport: URLSessionTransport())
         let response = try await client.listTrendingPois(query: .init(page: page, limit: limit, lat: coordinates?.latitude, lon: coordinates?.longitude),headers: .init(Accept_hyphen_Language: Locale.preferredLanguages.first ?? "en-US"))
         switch response {
         case .ok(let okResponse):
             switch okResponse.body {
-                
+            
             case .json(let jsonResponse):
                 let resolvedItem: [ResolvedItem] = jsonResponse.data.compactMap { item in
                     
-                    guard let id = item.id, let title = item.name, let subtitle = item.category, let lon = item.coordinates?.lon, let lat = item.coordinates?.lat, let rating = item.rating, let ratingCount = item.ratings_count, let trendingImage = item.trending_image_url else { return ResolvedItem.artwork }
-                    return ResolvedItem(id: id, title: title, subtitle: subtitle, category: subtitle, symbol: .pin, type: .hudhud, coordinate: CLLocationCoordinate2D(latitude: lat, longitude: lon), phone: nil, website: nil, rating: rating, ratingCount: ratingCount, trendingImage: trendingImage)
+                    guard let ratingCount = item.ratings_count else { return ResolvedItem.artwork }
+                    return ResolvedItem(id: item.id, title: item.name, subtitle: item.category, category: item.category, symbol: .pin, type: .hudhud, coordinate: CLLocationCoordinate2D(latitude: item.coordinates.lat, longitude: item.coordinates.lon), phone: nil, website: nil, rating: item.rating, ratingCount: ratingCount, trendingImage: item.trending_image_url)
                 }
                     return resolvedItem
                 
