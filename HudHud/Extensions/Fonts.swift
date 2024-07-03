8ec5382c2489b31b16e707e836ef80c7cf012eb8
Fatima Aljaber <fatima-aljaber@hotmail.com>
2024-07-03 15:28:38 +0300

    add font to xcode

---
+++ HudHud/Extensions/Fonts.swift
@@ -0,0 +1,70 @@
+//
+//  Fonts.swift
+//  HudHud
+//
+//  Created by Fatima Aljaber on 03/07/2024.
+//  Copyright © 2024 HudHud. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+// MARK: - FontWeight
+
+enum FontWeight {
+    case bold
+    case boldItalic
+    case extraBold
+    case extraBoldItalic
+    case extraLight
+    case extraLightItalic
+    case italic
+    case light
+    case lightItalic
+    case medium
+    case mediumItalic
+    case regular
+    case semiBold
+    case semiBoldItalic
+}
+
+extension Font {
+    static func customFont(_ weight: FontWeight, size: CGFloat) -> Font {
+        switch weight {
+        case .bold:
+            return .custom("PlusJakartaSans-Bold", size: size)
+        case .boldItalic:
+            return .custom("PlusJakartaSans-BoldItalic", size: size)
+        case .extraBold:
+            return .custom("PlusJakartaSans-ExtraBold", size: size)
+        case .extraBoldItalic:
+            return .custom("PlusJakartaSans-ExtraBoldItalic", size: size)
+        case .extraLight:
+            return .custom("PlusJakartaSans-ExtraLight", size: size)
+        case .extraLightItalic:
+            return .custom("PlusJakartaSans-ExtraLightItalic", size: size)
+        case .italic:
+            return .custom("PlusJakartaSans-Italic", size: size)
+        case .light:
+            return .custom("PlusJakartaSans-Light", size: size)
+        case .lightItalic:
+            return .custom("PlusJakartaSans-LightItalic", size: size)
+        case .medium:
+            return .custom("PlusJakartaSans-Medium", size: size)
+        case .mediumItalic:
+            return .custom("PlusJakartaSans-MediumItalic", size: size)
+        case .regular:
+            return .custom("PlusJakartaSans-Regular", size: size)
+        case .semiBold:
+            return .custom("PlusJakartaSans-SemiBold", size: size)
+        case .semiBoldItalic:
+            return .custom("PlusJakartaSans-SemiBoldItalic", size: size)
+        }
+    }
+}
+
+extension Text {
+    func customFont(_ fontWeight: FontWeight? = .regular, _ size: CGFloat? = nil) -> Text {
+        return self.font(.customFont(fontWeight ?? .regular, size: size ?? 16))
+    }
+}
