2b20f065be95a976a3d59742f190ff4afb92bbfa
Fatima Aljaber <fatima-aljaber@hotmail.com>
2024-07-06 17:21:51 +0300

    Update PresentationDetent.swift

--- HudHud/Extensions/PresentationDetent.swift
+++ HudHud/Extensions/PresentationDetent.swift
@@ -10,5 +10,17 @@ import SwiftUI
 
 extension PresentationDetent {
     static let small: PresentationDetent = .height(80)
-    static let third: PresentationDetent = .fraction(0.33)
+    static let third: PresentationDetent = {
+        let screenHeight = UIScreen.main.bounds.height
+        switch screenHeight {
+        case let height where height > 900:
+            return .fraction(0.33) // For larger screens like iPhone 14 Pro Max
+        case let height where height > 800:
+            return .fraction(0.30) // For medium screens like iPhone 14 Plus
+        case let height where height > 700:
+            return .fraction(0.25) // For smaller screens like iPhone 13/14
+        default:
+            return .fraction(0.2) // For very small screens
+        }
+    }()
 }
