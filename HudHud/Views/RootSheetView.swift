9af7d4ac77bcc35a9337e2861e7398d5e72f4052
Fatima Aljaber <fatima-aljaber@hotmail.com>
2024-07-22 11:29:23 +0300

    HHIOS-197-Improve-UI-of-the-navigationSheet (#154)
    * NavigationSheetUI
    
    - Navigation Sheet design
    - remove back button from add location sheet
    - Fix recents (add location)
    
    * edited
    
    after Naif review

--- HudHud/Views/RootSheetView.swift
+++ HudHud/Views/RootSheetView.swift
@@ -47,7 +47,7 @@ struct RootSheetView: View {
                             return tempStore
                         }()
                         SearchSheet(mapStore: freshSearchViewStore.mapStore,
-                                    searchStore: freshSearchViewStore, trendingStore: self.trendingStore)
+                                    searchStore: freshSearchViewStore, trendingStore: self.trendingStore).navigationBarBackButtonHidden()
                     case .favorites:
                         // Initialize fresh instances of MapStore and SearchViewStore
                         let freshMapStore = MapStore(motionViewModel: .storeSetUpForPreviewing)
