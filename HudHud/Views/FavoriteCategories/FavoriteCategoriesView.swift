d0459c8a25906ab8f2c99c2c4f45b5ba0d7202bf
AlloTheOne <Allo.busy@gmail.com>
2024-07-25 11:24:12 +0300

    HHIOS-214 Fix the coloring and design of favorites

--- HudHud/Views/FavoriteCategories/FavoriteCategoriesView.swift
+++ HudHud/Views/FavoriteCategories/FavoriteCategoriesView.swift
@@ -31,6 +31,7 @@ struct FavoriteCategoriesView: View {
                         }
                     } label: {
                         Text(favorite.type)
+                            .hudhudFont(size: 12, fontWeight: .medium)
                     }
                     .buttonStyle(FavoriteCategoriesButton(sfSymbol: favorite.getSymbol(type: favorite.type), tintColor: favorite.tintColor))
                 }
@@ -38,7 +39,8 @@ struct FavoriteCategoriesView: View {
                     self.viewMoreShown = true
                 } label: {
                     Text("Add")
-                }.buttonStyle(FavoriteCategoriesButton(sfSymbol: .plusCircleFill, tintColor: .green))
+                        .hudhudFont(size: 12, fontWeight: .medium)
+                }.buttonStyle(FavoriteCategoriesButton(sfSymbol: .plusCircleFill, tintColor: Color.Colors.General._10GreenMain))
             }
             Spacer()
         }
