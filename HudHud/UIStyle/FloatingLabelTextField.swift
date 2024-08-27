d16a0b60295db04061c0a30a2c31bdadcc0c406c
AlloTheOne <Allo.busy@gmail.com>
2024-08-27 14:25:53 +0300

    HHIOS-264 User Login

---
+++ HudHud/UIStyle/FloatingLabelTextField.swift
@@ -0,0 +1,19 @@
+//
+//  FloatingLabelTextField.swift
+//  HudHud
+//
+//  Created by Alaa . on 27/08/2024.
+//  Copyright © 2024 HudHud. All rights reserved.
+//
+
+import SwiftUI
+
+struct FloatingLabelTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FloatingLabelTextField()
+}
---
+++ HudHud/Views/UserLoginView.swift
@@ -0,0 +1,19 @@
+//
+//  UserLoginView.swift
+//  HudHud
+//
+//  Created by Alaa . on 27/08/2024.
+//  Copyright © 2024 HudHud. All rights reserved.
+//
+
+import SwiftUI
+
+struct UserLoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserLoginView()
+}
