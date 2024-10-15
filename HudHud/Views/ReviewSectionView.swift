7b3bcce5f256657c9a492371c6fc00a253a2a792
Fatima Aljaber <fatima-aljaber@hotmail.com>
2024-10-15 18:47:17 +0300

    image resizable

--- HudHud/Views/ReviewSectionView.swift
+++ HudHud/Views/ReviewSectionView.swift
@@ -31,8 +31,9 @@ struct ReviewSectionView: View {
                 .lineLimit(nil)
                 .foregroundStyle(Color.Colors.General._01Black)
 
+            ImageView(review: self.review)
+                .padding(.vertical, 2)
             BottomBar(isUseful: self.review.isUseful, usefulCount: self.review.usefulCount)
-                .padding(.top)
 
             Divider()
         }
@@ -95,39 +96,121 @@ struct RatingsView: View {
     }
 }
 
+// MARK: - ImageView
+
+struct ImageView: View {
+
+    // MARK: Properties
+
+    let review: Review
+
+    @State private var selectedMedia: URL?
+
+    // MARK: Content
+
+    var body: some View {
+        VStack {
+            if self.review.images.count == 1, let image = review.images.first {
+                AsyncImage(url: image) { image in
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: .infinity, height: 168)
+                        .clipped()
+                        .contentShape(.rect)
+                } placeholder: {
+                    ProgressView()
+                        .progressViewStyle(.automatic)
+                        .background(.secondary)
+                        .cornerRadius(10)
+                }
+                .background(.secondary)
+                .cornerRadius(10)
+                .frame(width: .infinity, height: 168)
+                .onTapGesture {
+                    self.selectedMedia = image
+                }
+            } else {
+                ScrollView(.horizontal) {
+                    HStack {
+                        ForEach(self.review.images, id: \.self) { mediaURL in
+                            AsyncImage(url: mediaURL) { image in
+                                image
+                                    .resizable()
+                                    .scaledToFit()
+                                    .scaledToFill()
+                                    .frame(width: 120, height: 120)
+                            } placeholder: {
+                                ProgressView()
+                                    .progressViewStyle(.automatic)
+                                    .frame(width: 120, height: 120)
+                                    .background(.secondary)
+                                    .cornerRadius(10)
+                            }
+                            .background(.secondary)
+                            .cornerRadius(10)
+                            .onTapGesture {
+                                self.selectedMedia = mediaURL
+                            }
+                        }
+                    }
+                }
+                .scrollIndicators(.hidden)
+            }
+        }
+        .sheet(item: self.$selectedMedia) { mediaURL in
+            FullPageImage(
+                mediaURL: mediaURL,
+                mediaURLs: self.review.images
+            )
+        }
+    }
+}
+
 // MARK: - BottomBar
 
 struct BottomBar: View {
 
     // MARK: Properties
 
-    let isUseful: Bool
+    @State var isUseful: Bool
     let usefulCount: Int
+
+    @State private var moreButtonPressed: Bool = false
 
     // MARK: Content
 
     var body: some View {
         HStack(spacing: 5) {
             Button {
+                self.isUseful.toggle()
                 // like the review
             }
             label: {
-                Image(systemSymbol: self.isUseful ? .heartFill : .heart)
-                    .foregroundStyle(self.isUseful ? Color.Colors.General._06DarkGreen : Color.Colors.General._02Grey)
-            }
+                HStack(spacing: 5) {
+                    Image(systemSymbol: self.isUseful ? .heartFill : .heart)
+                        .foregroundStyle(self.isUseful ? Color.Colors.General._06DarkGreen : Color.Colors.General._02Grey)
 
-            Text("Useful(\(self.usefulCount))")
-                .hudhudFont(.footnote)
-                .foregroundStyle(self.isUseful ? Color.Colors.General._06DarkGreen : Color.Colors.General._02Grey)
+                    Text(self.isUseful ? "Useful (\(self.usefulCount))" : "UseFul")
+                        .hudhudFont(.footnote)
+                        .foregroundStyle(self.isUseful ? Color.Colors.General._06DarkGreen : Color.Colors.General._02Grey)
+                }
+            }
 
             Spacer()
 
             Button {
-                // naivgate to more view
+                self.moreButtonPressed.toggle()
             }
             label: {
                 Image(systemSymbol: .ellipsis)
+                    .foregroundStyle(Color.Colors.General._02Grey)
             }
+        }
+        .confirmationDialog("", isPresented: self.$moreButtonPressed) {
+            Button("Share") {}
+            Button("Report", role: .destructive) {}
+            Button("Cancel", role: .cancel) {}
         }
     }
 }
@@ -139,8 +222,8 @@ struct BottomBar: View {
         rating: 4,
         date: "12 September 2024",
         reviewText: "Amazing blend of authentic Moroccan flavors with warm hospitality, making for an unforgettable dining experience.",
-        images: [URL(string: "https://img.freepik.com/free-photo/delicious-arabic-fast-food-skewers-black-plate_23-2148651145.jpg?w=740&t=st=1708506411~exp=1708507011~hmac=e3381fe61b2794e614de83c3f559ba6b712fd8d26941c6b49471d500818c9a77")!, URL(string: "https://img.freepik.com/free-photo/delicious-arabic-fast-food-skewers-black-plate_23-2148651145.jpg?w=740&t=st=1708506411~exp=1708507011~hmac=e3381fe61b2794e614de83c3f559ba6b712fd8d26941c6b49471d500818c9a77")!, URL(string: "https://img.freepik.com/free-photo/delicious-arabic-fast-food-skewers-black-plate_23-2148651145.jpg?w=740&t=st=1708506411~exp=1708507011~hmac=e3381fe61b2794e614de83c3f559ba6b712fd8d26941c6b49471d500818c9a77")!],
-        isUseful: true,
+        images: [URL(string: "https://img.freepik.com/free-photo/delicious-arabic-fast-food-skewers-black-plate_23-2148651145.jpg?w=740&t=st=1708506411~exp=1708507011~hmac=e3381fe61b2794e614de83c3f559ba6b712fd8d26941c6b49471d500818c9a77")!],
+        isUseful: false,
         usefulCount: 15
     )
     ReviewSectionView(review: review)
