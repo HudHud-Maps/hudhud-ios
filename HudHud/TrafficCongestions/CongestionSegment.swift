16b785828f0339b75c5e55593c09aede623046ff
Ali A. Hilal <ali94_200970@yahoo.com>
2024-10-17 09:56:10 +0300

    Removed unnecessary files

---
+++ HudHud/TrafficCongestions/CongestionSegment.swift
@@ -0,0 +1,14 @@
+//
+//  CongestionSegment.swift
+//  HudHud
+//
+//  Created by Ali Hilal on 17/10/2024.
+//  Copyright © 2024 HudHud. All rights reserved.
+//
+
+import CoreLocation
+
+struct CongestionSegment {
+    let level: String
+    let geometry: [CLLocationCoordinate2D]
+}
