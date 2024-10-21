4788657262ddb2f8bed59d058d822b7c9e6dd6da
AlloTheOne <Allo.busy@gmail.com>
2024-10-21 08:50:51 +0300

    HHIOS-327 Photo Viewer

--- HudHud/Views/POISheet/POIMediaView.swift
+++ HudHud/Views/POISheet/POIMediaView.swift
@@ -49,7 +49,7 @@ struct POIMediaView: View {
             .padding(.leading)
         }
         .scrollIndicators(.hidden)
-        .sheet(item: self.$selectedMedia) { mediaURL in
+        .fullScreenCover(item: self.$selectedMedia) { mediaURL in
             FullPageImage(
                 mediaURL: mediaURL,
                 mediaURLs: self.mediaURLs
@@ -67,35 +67,106 @@ struct FullPageImage: View {
     @State var mediaURL: URL
     let mediaURLs: [URL]
     @Environment(\.dismiss) var dismiss
+    @State var actionSheetShown: Bool = false
+    @State var isBackendReady: Bool = false
 
     // MARK: Content
 
     var body: some View {
         NavigationStack {
-            TabView(selection: self.$mediaURL) {
-                ForEach(self.mediaURLs, id: \.self) { mediaURL in
-                    AsyncImage(url: mediaURL) { image in
-                        image
-                            .resizable()
-                            .scaledToFit()
-                    } placeholder: {
-                        ProgressView()
-                            .progressViewStyle(.automatic)
-                    }
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .tag(mediaURL)
+            ZStack {
+                // blur background
+                AsyncImage(url: self.mediaURL) { image in
+                    image
+                        .resizable()
+                        .blur(radius: 20, opaque: true)
+                        .overlay(Color.black.opacity(0.6))
+                        .ignoresSafeArea()
+                } placeholder: {
+                    ProgressView()
+                        .progressViewStyle(.automatic)
                 }
-            }
-            .tabViewStyle(.page(indexDisplayMode: .always))
-            .background(.black)
-            .toolbar {
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button {
-                        self.dismiss()
-                    } label: {
-                        Image(systemSymbol: .xCircleFill)
-                            .tint(.gray)
+
+                // Main content
+                VStack {
+                    TabView(selection: self.$mediaURL) {
+                        ForEach(self.mediaURLs, id: \.self) { mediaURL in
+                            VStack(spacing: 0.0) {
+                                AsyncImage(url: mediaURL) { image in
+                                    image
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                } placeholder: {
+                                    ProgressView()
+                                        .progressViewStyle(.automatic)
+                                }
+                                .tag(mediaURL)
+
+                                Spacer()
+                            }
+                        }
                     }
+                    .tabViewStyle(.page(indexDisplayMode: .never))
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+
+                    VStack {
+                        HStack {
+                            if self.isBackendReady {
+                                Image(systemSymbol: .personCircleFill)
+                                    .font(.largeTitle)
+                                    .foregroundColor(.white)
+                                VStack(alignment: .leading) {
+                                    Text("Patrick")
+                                        .font(.subheadline)
+                                        .foregroundColor(.white)
+                                        .padding(.top, 8)
+                                    Text("12 September 2024")
+                                        .font(.caption)
+                                        .foregroundColor(.gray)
+                                }
+                            }
+                            Spacer()
+                            Button {
+                                self.actionSheetShown = true
+                            } label: {
+                                Image(systemSymbol: .ellipsis)
+                                    .font(.title)
+                                    .foregroundColor(.white)
+                            }
+                        }
+                        .padding()
+                    }
+                    .frame(maxWidth: .infinity, alignment: .bottom)
+                }
+
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        Button {
+                            self.dismiss()
+                        } label: {
+                            Text("Close")
+                                .hudhudFont()
+                                .tint(.white)
+                        }
+                    }
+
+                    ToolbarItem(placement: .principal) {
+                        if let index = mediaURLs.firstIndex(of: mediaURL) {
+                            Text("\(index + 1) of \(self.mediaURLs.count)")
+                                .hudhudFont()
+                                .foregroundColor(.white)
+                        }
+                    }
+
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Image(systemSymbol: .cameraFill)
+                            .foregroundStyle(Color.Colors.General._06DarkGreen)
+                    }
+                }
+                .confirmationDialog("action", isPresented: self.$actionSheetShown) {
+                    Button("Share") {}
+                    Button("Report", role: .destructive) {}
                 }
             }
         }
