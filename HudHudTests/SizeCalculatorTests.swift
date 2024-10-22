59382ea89d58088e6f51d1c586f9264480772d17
Patrick Kladek <patrick.kladek@outlook.com>
2024-10-22 17:18:15 +0400

    remove duplicated tests

--- HudHudTests/SizeCalculatorTests.swift
+++ HudHudTests/SizeCalculatorTests.swift
@@ -24,17 +24,4 @@ struct SizeCalculatorTests {
         #expect(clipped.width == 6752)
         #expect(clipped.height == 3376)
     }
-
-    // Only for Simulator, real device has higher limits
-    @Test(.enabled(if: UIApplication.isSimulator), .enabled(if: DeviceSupport.model == .iPhone15Pro), arguments: [
-        CGSize(width: 3376, height: 1688),
-        CGSize(width: 6752, height: 3376),
-        CGSize(width: 13504, height: 6752),
-        CGSize(width: 27008, height: 13504)
-    ])
-    func imageClippingiPhone15Pro(size: CGSize) throws {
-        let clipped = size.clipToMaximumSupportedTextureSize()
-        #expect(clipped.width == 3376)
-        #expect(clipped.height == 1688)
-    }
 }
