d0bf315913e8faaaaeac85193f2d5c0472003333
Patrick Kladek <patrick.kladek@outlook.com>
2024-10-25 17:00:14 +0400

    run release-notes only on release builds

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -4,5 +4,10 @@ set -euo pipefail
 defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
 
-../.tools/release-notes linear testflight --file ../TestFlight/WhatToTest.en-US.txt --api-key $LINEAR_API_KEY --team "iOS"
+if [ $CI_WORKFLOW = 'Internal TestFlight' ]
+then
+	../.tools/release-notes linear testflight --file ../TestFlight/WhatToTest.en-US.txt --api-key $LINEAR_API_KEY --team "iOS"
+fi
+
+
 
--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,1 +1,7 @@
-../.tools/release-notes linear move-tickets --api-key $LINEAR_API_KEY --team "iOS"
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [ $CI_WORKFLOW = 'Internal TestFlight' ]
+then
+	../.tools/release-notes linear move-tickets --api-key $LINEAR_API_KEY --team "iOS"
+fi
