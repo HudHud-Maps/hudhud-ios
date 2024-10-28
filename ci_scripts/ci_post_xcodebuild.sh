ff3275b3783760880d894173557c9a1fb0839812
Patrick Kladek <patrick.kladek@outlook.com>
2024-10-28 18:24:48 +0400

    run signed app check only for testflight runs

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -7,23 +7,31 @@ then
 	exit 1
 fi
 
-if [[ -n $CI_APP_STORE_SIGNED_APP_PATH ]]; # checks if there is an AppStore signed archive after running xcodebuild
+
+VERSION=$(cat ../${CI_PRODUCT}.xcodeproj/project.pbxproj | grep -m1 'MARKETING_VERSION' | cut -d'=' -f2 | tr -d ';' | tr -d ' ')
+
+if [[ $CI_WORKFLOW = 'Internal TestFlight' ]]
 then
 	
-	VERSION=$(cat ../${CI_PRODUCT}.xcodeproj/project.pbxproj | grep -m1 'MARKETING_VERSION' | cut -d'=' -f2 | tr -d ';' | tr -d ' ')
-	
-	if [[ $CI_WORKFLOW = 'Internal TestFlight' ]]
+	if [[ -n $CI_APP_STORE_SIGNED_APP_PATH ]]; # checks if there is an AppStore signed archive after running xcodebuild
 	then
+		
 		../.tools/release-notes linear move-tickets --api-key $LINEAR_API_KEY --team "iOS"
 		git tag TestFlight/Internal/$VERSION\($CI_BUILD_NUMBER\)
 		git push --tags "https://${GIT_AUTH}@github.com/HudHud-Maps/hudhud-ios.git"
+
 	fi
+fi
+
+if [[ $CI_WORKFLOW = 'External TestFlight' ]]
+then
 	
-	if [[ $CI_WORKFLOW = 'External TestFlight' ]]
+	if [[ -n $CI_APP_STORE_SIGNED_APP_PATH ]]; # checks if there is an AppStore signed archive after running xcodebuild
 	then
+		
 		../.tools/release-notes linear move-tickets --api-key $LINEAR_API_KEY --team "iOS" --column "Available on TestFlight" --destination "Done"
 		git tag TestFlight/External/$VERSION\($CI_BUILD_NUMBER\)
 		git push --tags "https://${GIT_AUTH}@github.com/HudHud-Maps/hudhud-ios.git"
-	fi
 		
+	fi
 fi
