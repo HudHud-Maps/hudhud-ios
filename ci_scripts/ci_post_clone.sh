072b5ca778fcac2bc05d1c2b50766a3a0171c913
Patrick Kladek <patrick.kladek@outlook.com>
2024-10-29 19:30:07 +0400

    HHIOS-344 Release internal TestFlight builds on merging develop (#279)
    * fix tests
    
    * fix warnings
    
    * add release notes
    
    * add release notes automation
    
    * fix typo
    
    * run release-notes only on release builds
    
    * remove release notes xcode reference
    
    * add tag after successful deployment
    
    * only push tag for specific workflow
    
    * run signed app check only for testflight runs
    
    * add alias to project in ci_scripts folder
    
    * log env
    
    * Rename Testplans
    
    * speed up build to reduce feedback loop
    
    * add alias
    
    * log current directory
    
    * try parent folder for project lookup
    
    * log version
    
    * skip build validation
    
    * Revert "speed up build to reduce feedback loop"
    
    This reverts commit 8cc860272f4ca06986d4536daf866ff3393f62ea.
    
    * add new script
    
    * add log messages
    
    * create real symbolic link
    
    * create x86 & arm binary of release-notes
    
    * fix failing tests

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,3 +3,11 @@ set -euo pipefail
 
 defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
+
+if [ $CI_WORKFLOW = 'Internal TestFlight' ]
+then
+	../.tools/release-notes linear testflight --file ../TestFlight/WhatToTest.en-US.txt --api-key $LINEAR_API_KEY --team "iOS"
+fi
+
+
+
