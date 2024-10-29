af87a743788a6e373e39bae929595ece5313fa10
Fatima Aljaber <fatima-aljaber@hotmail.com>
2024-10-29 11:36:56 +0300

    add photo button

--- HudHud/Views/POISearchView/PhotoTabView.swift
+++ HudHud/Views/POISearchView/PhotoTabView.swift
@@ -20,44 +20,61 @@ struct PhotoTabView: View {
     // MARK: Content
 
     var body: some View {
-        ScrollView {
-            PhotosView(items: self.item.mediaURLs, id: \.self, spacing: 5) { url in
-                AsyncImage(url: url) { phase in
-                    switch phase {
-                    case let .success(image):
-                        ZStack(alignment: .bottomTrailing) {
-                            image
-                                .resizable()
-                                .onTapGesture {
-                                    self.selectedMedia = url
-                                }
-                            // Display an image label, styled and positioned at bottom-right
-                            Text(self.item.title)
-                                .hudhudFontStyle(.labelSmall)
-                                .padding(8)
-                                .background(Color.Colors.General._01Black.opacity(0.5))
-                                .foregroundColor(Color.Colors.General._05WhiteBackground)
-                                .clipShape(.rect(topLeadingRadius: 8))
+        ZStack(alignment: .bottomTrailing) {
+            ScrollView {
+                PhotosView(items: self.item.mediaURLs, id: \.self, spacing: 5) { url in
+                    AsyncImage(url: url) { phase in
+                        switch phase {
+                        case let .success(image):
+                            ZStack(alignment: .bottomTrailing) {
+                                image
+                                    .resizable()
+                                    .onTapGesture {
+                                        self.selectedMedia = url
+                                    }
+                                // Display an image label, styled and positioned at bottom-right
+                                Text(self.item.title)
+                                    .hudhudFontStyle(.labelSmall)
+                                    .padding(8)
+                                    .background(Color.Colors.General._01Black.opacity(0.5))
+                                    .foregroundColor(Color.Colors.General._05WhiteBackground)
+                                    .clipShape(.rect(topLeadingRadius: 8))
+                            }
+                        case .failure:
+                            Color.gray
+                        case .empty:
+                            ProgressView()
+                                .cornerRadius(7.0)
+                                .progressViewStyle(.automatic)
+                                .frame(width: 96, height: 96)
+                        @unknown default:
+                            Color.gray
                         }
-                    case .failure:
-                        Color.gray
-                    case .empty:
-                        ProgressView()
-                            .cornerRadius(7.0)
-                            .progressViewStyle(.automatic)
-                            .frame(width: 96, height: 96)
-                    @unknown default:
-                        Color.gray
                     }
                 }
             }
-        }
-        .scrollIndicators(.hidden)
-        .sheet(item: self.$selectedMedia) { mediaURL in
-            FullPageImage(
-                mediaURL: mediaURL,
-                mediaURLs: self.item.mediaURLs
-            )
+            .scrollIndicators(.hidden)
+            .sheet(item: self.$selectedMedia) { mediaURL in
+                FullPageImage(
+                    mediaURL: mediaURL,
+                    mediaURLs: self.item.mediaURLs
+                )
+            }
+            Button(action: {
+                //  add photo
+            }, label: {
+                ZStack {
+                    Circle()
+                        .fill(Color.Colors.General._06DarkGreen)
+                        .frame(width: 56, height: 56)
+                    Image(.addPhoto)
+                        .resizable()
+                        .renderingMode(.template)
+                        .frame(width: 24, height: 24)
+                        .foregroundColor(Color.Colors.General._05WhiteBackground)
+                }
+            })
+            .padding([.bottom, .trailing], 16)
         }
     }
 }
