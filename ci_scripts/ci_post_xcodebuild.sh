0a7a522a18eb0451955dc7703e52e2ed9c06bc70
Patrick Kladek <patrick.kladek@outlook.com>
2024-10-29 17:30:24 +0400

    add log messages

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -3,11 +3,12 @@ set -euo pipefail
 
 if [ ${CI_XCODEBUILD_EXIT_CODE} != 0 ]
 then
+	echo "Previous Build step failed, aborting"
 	exit 1
 fi
 
 VERSION=$(cat ../HudHud.xcodeproj/project.pbxproj | grep -m1 'MARKETING_VERSION' | cut -d'=' -f2 | tr -d ';' | tr -d ' ')
-echo "Build Version:" $VERSION
+echo "Found Build Version:" $VERSION
 
 
 if [[ $CI_WORKFLOW = 'Internal TestFlight' ]]
@@ -16,8 +17,13 @@ then
 	if [[ -n $CI_APP_STORE_SIGNED_APP_PATH ]]; # checks if there is an AppStore signed archive after running xcodebuild
 	then
 		
+		echo "Running Internal TestFlight"
+		echo "moving tickets to 'Available on TestFlight' column"
 		../.tools/release-notes linear move-tickets --api-key $LINEAR_API_KEY --team "iOS"
-		git tag TestFlight/Internal/$VERSION\($CI_BUILD_NUMBER\)
+		
+		NEW_TAG=TestFlight/Internal/$VERSION\($CI_BUILD_NUMBER\)
+		echo "Tag commit with '$NEW_TAG'"
+		git tag $NEW_TAG
 		git push --tags "https://${GIT_AUTH}@github.com/HudHud-Maps/hudhud-ios.git"
 
 	fi
@@ -29,8 +35,13 @@ then
 	if [[ -n $CI_APP_STORE_SIGNED_APP_PATH ]]; # checks if there is an AppStore signed archive after running xcodebuild
 	then
 		
+		echo "Running External TestFlight"
+		echo "moving tickets to 'Done' column"
 		../.tools/release-notes linear move-tickets --api-key $LINEAR_API_KEY --team "iOS" --column "Available on TestFlight" --destination "Done"
-		git tag TestFlight/External/$VERSION\($CI_BUILD_NUMBER\)
+		
+		NEW_TAG=TestFlight/External/$VERSION\($CI_BUILD_NUMBER\)
+		echo "Tag commit with '$NEW_TAG'"
+		git tag $NEW_TAG
 		git push --tags "https://${GIT_AUTH}@github.com/HudHud-Maps/hudhud-ios.git"
 		
 	fi
