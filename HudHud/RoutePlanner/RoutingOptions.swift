4d579fdd833bd2218978361791fde9bee7d38c0c
Ali A. Hilal <ali94_200970@yahoo.com>
2024-10-29 11:20:26 +0100

    Refactor RoutingService and RoutePlanner
    - Extract the RoutingService protocol from RoutingService.swift to its own file.
    - Move the RoutingOptions struct to its own file.
    - Update imports in RoutingService.swift and RoutePlanner.swift.

--- HudHud/RoutePlanner/RoutingOptions.swift
+++ HudHud/RoutePlanner/RoutingOptions.swift
@@ -7,3 +7,66 @@
 //
 
 import Foundation
+
+struct RoutingOptions {
+
+    // MARK: Nested Types
+
+    enum Geometry: String {
+        case polyline
+        case polyline6
+        case geojson
+    }
+
+    enum Overview: String {
+        case simplified
+        case full
+        case `false`
+    }
+
+    enum Annotation: String, CaseIterable {
+        case duration
+        case distance
+        case speed
+        case congestion
+    }
+
+    enum VoiceUnits: String {
+        case imperial
+        case metric
+    }
+
+    // MARK: Properties
+
+    var accessToken: String = ""
+    var alternatives: Bool = false
+    var geometries: Geometry = .polyline6
+    var overview: Overview = .full
+    var steps: Bool = true
+    var continueStraight: Bool = true
+    var annotations: Set<Annotation> = [.congestion, .distance]
+    var language: String = Locale.preferredLanguages.first ?? "en-US"
+    var roundaboutExits: Bool = true
+    var voiceInstructions: Bool = true
+    var bannerInstructions: Bool = true
+    var voiceUnits: VoiceUnits = .metric
+
+    // MARK: Computed Properties
+
+    var queryItems: [URLQueryItem] {
+        [
+            URLQueryItem(name: "access_token", value: self.accessToken),
+            URLQueryItem(name: "alternatives", value: self.alternatives.description),
+            URLQueryItem(name: "geometries", value: self.geometries.rawValue),
+            URLQueryItem(name: "overview", value: self.overview.rawValue),
+            URLQueryItem(name: "steps", value: self.steps.description),
+            URLQueryItem(name: "continue_straight", value: self.continueStraight.description),
+            URLQueryItem(name: "annotations", value: self.annotations.map(\.rawValue).joined(separator: ",")),
+            URLQueryItem(name: "language", value: self.language),
+            URLQueryItem(name: "roundabout_exits", value: self.roundaboutExits.description),
+            URLQueryItem(name: "voice_instructions", value: self.voiceInstructions.description),
+            URLQueryItem(name: "banner_instructions", value: self.bannerInstructions.description),
+            URLQueryItem(name: "voice_units", value: self.voiceUnits.rawValue)
+        ]
+    }
+}
--- HudHud/RoutePlanner/RoutingService.swift
+++ HudHud/RoutePlanner/RoutingService.swift
@@ -6,4 +6,14 @@
 //  Copyright © 2024 HudHud. All rights reserved.
 //
 
+import CoreLocation
+import FerrostarCoreFFI
 import Foundation
+
+protocol RoutingService {
+    func calculateRoute(
+        from start: Waypoint,
+        to end: Waypoint,
+        passingBy waypoints: [Waypoint]
+    ) async throws -> [Route]
+}
