c51a231bb2560a9268fa0e44f020d62a1f3fce12
Patrick Kladek <patrick.kladek@outlook.com>
2024-10-29 19:54:28 +0400

    fix release notes

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -4,10 +4,12 @@ set -euo pipefail
 defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
 
-if [ $CI_WORKFLOW = 'Internal TestFlight' ]
+if [[ $CI_WORKFLOW = 'Internal TestFlight' ]]
 then
 	../.tools/release-notes linear testflight --file ../TestFlight/WhatToTest.en-US.txt --api-key $LINEAR_API_KEY --team "iOS"
 fi
 
-
-
+if [[ $CI_WORKFLOW = 'External TestFlight' ]]
+then
+	../.tools/release-notes linear testflight --file ../TestFlight/WhatToTest.en-US.txt --api-key $LINEAR_API_KEY --team "iOS" --column "Available on TestFlight"
+fi
