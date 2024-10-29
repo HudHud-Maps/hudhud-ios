43774339267d70522966cb61c2aefa8b73056c6e
Patrick Kladek <patrick.kladek@outlook.com>
2024-10-29 17:03:17 +0400

    add new script

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 set -euo pipefail
 
-#if [ ${CI_XCODEBUILD_EXIT_CODE} != 0 ]
-#then
-#	exit 1
-#fi
+if [ ${CI_XCODEBUILD_EXIT_CODE} != 0 ]
+then
+	exit 1
+fi
 
 VERSION=$(cat ../HudHud.xcodeproj/project.pbxproj | grep -m1 'MARKETING_VERSION' | cut -d'=' -f2 | tr -d ';' | tr -d ' ')
-echo "Build Version: " $VERSION
+echo "Build Version:" $VERSION
+
 
 if [[ $CI_WORKFLOW = 'Internal TestFlight' ]]
 then
