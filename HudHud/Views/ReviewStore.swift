cb8074f4d08c03a66635f572fccd6394ba6e3ed8
Alaa Alabdullah <79634177+AlloTheOne@users.noreply.github.com>
2024-11-04 12:26:08 +0300

    Update HudHud/Views/ReviewStore.swift
    Co-authored-by: Patrick Kladek <patrick.kladek@outlook.com>

--- HudHud/Views/ReviewStore.swift
+++ HudHud/Views/ReviewStore.swift
@@ -24,7 +24,7 @@ struct Review: Identifiable {
 }
 
 extension Review {
-    static var reviewForPreview: Review = .init(
+    static var reviewForPreview = Review(
         username: "Ahmad Kamal",
         userType: "Trip Advisor",
         userImage: URL(string: "https://img.freepik.com/free-photo/delicious-arabic-fast-food-skewers-black-plate_23-2148651145.jpg?w=740&t=st=1708506411~exp=1708507011~hmac=e3381fe61b2794e614de83c3f559ba6b712fd8d26941c6b49471d500818c9a77") ?? defaultImageURL(),
