a527544e2dc5701c99c6428c613a58095f3cfe2b
Patrick Kladek <patrick.kladek@outlook.com>
2024-11-07 15:38:55 +0800

    add SwiftLint

--- .tools/ensure_swiftlint.sh
+++ .tools/ensure_swiftlint.sh
@@ -3,7 +3,7 @@
 if grep -q 'productName = "plugin:SwiftLint";' ../HudHud.xcodeproj/project.pbxproj; then
 	echo "✅ SwiftLint part of build pipeline"
 else
-	echo "SwiftLint not part of build pipeline."
-	echo "Please add it in the project settings under: HudHud -> Build Phases -> Run Build Tool Plug-ins"
+	echo "SwiftLint is not part of the build pipeline."
+	echo "Please add it via the project settings under: HudHud -> Build Phases -> Run Build Tool Plug-ins"
 	exit 1
 fi
